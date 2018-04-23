1f5e99de51c6818d28ed473c533204beeec9f0ca
Terencio Agozzino <terencio.agozzino@gmail.com>
2018-04-23 12:20:26 +0200

    Add refactoring for tmp values

--- src/scripter.py
+++ src/scripter.py
@@ -54,10 +54,10 @@ class Scripter:
         with open(filename, "w") as dest_file:
             for template in templates:
                 with open(template, 'r') as template_file:
-                    temp_value = template_file.read()
+                    tmp = template_file.read()
                     if log:
-                        print(temp_value)
-                    dest_file.write(temp_value)
+                        print(tmp)
+                    dest_file.write(tmp)
 
     def get_rafters(self, filename):
         """Gets words from a file between rafters.
